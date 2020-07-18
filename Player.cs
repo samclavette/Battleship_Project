7830b15347a0ef05c8f12285dfc63d409ccac215
Sam Clavette <samclavette@yahoo.com>
2020-07-18 15:51:00 -0500

    Added member variables and a constructor for Ships class. I also created a gameboard array in the Player class.

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Player
+    {
+        public string name;
+        List<Ships> shipList;
+        int[,] gameboard = new int[20, 20];
+    }
+}
---
+++ Ships.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Ships
+    {
+        public string name;
+        public int shipSize;
+
+        public Ships(string name, int shipSize)
+        {
+            this.name = name;
+            this.shipSize = shipSize;
+        }
+    }
+}
